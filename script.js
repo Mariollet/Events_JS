b0caa247f75490b42ff9fae57a27781bb592e47e
Azzerty23 <azzerty23@hotmail.fr>
2019-11-18 13:33:56 +0100

    Fonctionnalité 1-bis

--- script.js
+++ script.js
@@ -1,2 +1,9 @@
+// Fonctionnalité 1
+
 const footer = document.getElementsByTagName("footer")[0];
-footer.addEventListener("click", () => {console.log("Clique")})
+footer.addEventListener("click", () => {console.log("Clique")});
+
+// Fonctionnalité 1 - bis
+
+let counter = 0;
+footer.addEventListener("click", () => {console.log(`Clique ${counter++}`)});
